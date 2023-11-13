2a49319cafd56cce52d9eca13a0dd2f5891d65a4
jmduranse <86153959+jmduranse@users.noreply.github.com>
2023-11-13 14:18:34 +0100

    Add files via upload

--- userbk.sh
+++ userbk.sh
@@ -1,4 +1,5 @@
-#10/05/2022 
+#!/bin/bash
+#24/05/2022 
 #
 #Juan M. Duran para eac3
 #
@@ -13,41 +14,54 @@ ERR_BACKUP=4
 ERR_BADUSER=5
 ERR_BADHOME=6
 ERR_DELETION=7
-
+ERR_BACKUPDIR=8
+ERR_BADRETEN=9
+status=0
 
 limit=$1
 user=$2
 directory=$3
-
+homedir=""
 
 status=0
 TAR=$(which tar)
 
 
 #function for backup
+# Parameters : backup [user] [directory to store the file]
+
 
 
 backup () {
 
-	local limit=$1
-	local user=$2
-	local directory=$3
+	local user=$1
+	local directory=$2
 	local date=$(date +"%Y-%m-%d")
 	local time=$(date +%H%M)
+	local tarfile
 
+
+	if [ ! -d "$directory" ] ; then
+		echo "$PROG Error: We cant reach $directory for backup"  >> /dev/stderr
+		return $ERR_BACKUPDIR
+	fi
 
 	#compress directory with tar
-	tar cvzf $directory/$user-$date-$time.tgz $home
+	tarfile=$directory/$user-$date-$time.tgz
+	$TAR -zcPf "$tarfile" "$homedir"
 
 	#test if file generation was correct
-	if [ $? -ne 0 ]: then
-		echo "$PROG: An error has happened during the backup file generation"
+	if [ $? -ne 0 ]; then
+		echo "$PROG: An error has happened during the backup file generation"  >> /dev/stderr
 		return $ERR_BACKUP
+	else
+		echo "$tarfile has been generated"
 	fi
-return 0
+	return 0
 }
 
 #function to remove old backups
+# Parameters : backup [number of retentions] [user] [directory to store the file]
 
 purge () {
 
@@ -55,25 +69,24 @@ purge () {
 	local user=$2
 	local directory=$3
 	local file
-	local filenumber=0
+
 	local counter=0
 	local status=0
 
-	filenumber=$(find "$directory" -iname "$user*.tgz" |wc -l)
+	if [ "$(find "$directory" -iname "$user*.tgz" |wc -l)" -gt $limit ]; then
 
-	if [ $filenumber -gt $limit ]; then
-		while [ $filenumber -gt $limit ]
+		while [ "$(find "$directory" -iname "$user*.tgz" |wc -l)" -gt $limit ]
 		do
 			file=$(find "$directory" -iname "$user*.tgz"|sort|head -n 1)
-			rm $file 
-				if [ $? -ne 0 ]; then			
-					echo "Error deleting $file" #an error does not stop the loop. We keep deleting files
-					status=$ERR_DELETION
+			rm "$file" 
+				if [ $? -ne 0 ]; then
+					echo "Error deleting $file"  >> /dev/stderr
+					return  $ERR_DELETION
 				else
 					((counter++))
-				fi      
+				fi
 		done
-		echo "$PROG: $counter files deleted succesfully"    
+		echo "$PROG: $counter files deleted succesfully"
 	else
 		echo "$PROG: No files deleted"    
 	fi
@@ -87,7 +100,7 @@ purge () {
 #test if we are root 
 
 if [ $UID -ne 0 ]; then
-    echo "ERROR $PROG :Must be root to execute script"
+    echo "ERROR $PROG :Must be root to execute script"  >> /dev/stderr
     exit $ERR_NOROOT
 fi
 
@@ -95,65 +108,67 @@ fi
 
 
 if [ -z "$TAR" ]; then
-    echo "Error $PROG: tar executable not found."
-    exit $ERR_NOTAR
+    echo "Error $PROG: tar executable not found."  >> /dev/stderr
+    exit $ERR_NOTAR 
 fi
 
 
 #number of argument control
-if [$# -eq 3]; then
+if [ $# -eq 3 ]; then
 
 #number or retention control
-    if [ $limit -ge 1 && $limit -le 12 ]; then
+    if [[ $limit -ge 1 && $limit -le 12 ]]; then
 
 #existence of user control
-        if id "$user"  > /dev/null 2>&1 ; then;
+        if id "$user"  > /dev/null 2>&1 ; then
         homedir=$(grep "^$user:" /etc/passwd | cut -d ":" -f6)
 
 #home directory is correct control
-            if [ -d $homedir ]; then      
+            if [ -d $homedir ]; then
 
                 #call the backup function
-                backup $limit $user $directory  
-				
-				if [ $? -ne 0]; then
-				
-					echo "$PROG: ERROR during the backup file generation"
-					exit $?  #FIX THIS
-				
-				else
-					#call the deletion function			
+                backup $user $directory
+				status=$?
+
+				if [ $status -eq 0 ]; then
+
+					#call the deletion function
 					purge $limit $user $directory 
-				
-					if [ $? -ne 0]; then
-						echo "$PROG: ERROR during deletion of old backups"
-						exit $?  #backup and deletion of extra files has happened but in case of errors during the process exit code is not 0
+					status=$? 
+
+					if [ $status -ne 0 ]; then
+						#deletion of extra files has happened but in case of errors during the process exit code is not 0
+						echo ERROR  "$PROG: Could not delete some of the old backups"  >> /dev/stderr
 					fi
-				
+
+				else
+
+					echo "$PROG: ERROR during the backup file generation" >> /dev/stderr
+					exit $status
+				fi
             else
-				echo "ERROR: $PROG.  The home directory for that user does not exist or is unreachable"
-				echo "USAGE userbk.sh [number retentions (1-12)] [user] [target directory]"
+				echo "ERROR: $PROG.  The home directory for that user does not exist or is unreachable"  >> /dev/stderr
+				echo "USAGE: userbk.sh [number retentions (1-12)] [user] [target directory]"
 				exit $ERR_BADHOME
-        fi   
+
             fi
 
         else
-            echo "ERROR: $PROG.  The specified user do not exist"
-			echo "USAGE userbk.sh [number retentions (1-12)] [user] [target directory]"
+            echo "ERROR: $PROG.  The specified user do not exist"  >> /dev/stderr
+			echo "USAGE: userbk.sh [number retentions (1-12)] [user] [target directory]"
 			exit $ERR_BADUSER
         fi
 
     else
-        echo "ERROR: $PROG.  Specified number of backups to keep is to low or to high. "
-		echo "USAGE userbk.sh [number retentions (1-12)] [user] [target directory]"
-		exit $ERR_BADPARM
+        echo "ERROR: $PROG.  Specified number of backups to keep is too low or too high. "  >> /dev/stderr
+		echo "USAGE: userbk.sh [number retentions (1-12)] [user] [target directory]"
+		exit $ERR_BADRETEN
     fi
 
 else
-    echo "ERROR: $PROG.  Incorrect number of parameters"
-	echo "USAGE userbk.sh [number retentions (1-12)] [user] [target directory]"
-	exit $ERR_BADPARM
+    echo "ERROR: $PROG.  Incorrect number of parameters"  >> /dev/stderr
+    echo "USAGE: userbk.sh [number retentions (1-12)] [user] [target directory]"
+    exit $ERR_BADPARM
 fi
 
-exit 0
-
+exit $status
